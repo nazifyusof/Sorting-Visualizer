6e73dce1116e05092a4d4383ee3c659845f3dbb9
Nazif Yusof <nazifyusof@gmail.com>
2020-03-27 15:11:37 -0400

    Updates

--- precache-manifest.114f4d4065225a52e90528f06b68c1bb.js
+++ precache-manifest.114f4d4065225a52e90528f06b68c1bb.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "10a9c573cb77e85d7c57be5d28bedc1d",
+    "revision": "4a55b4a15fc98df4edd5adb804b3b9f5",
     "url": "/Sorting-Visualizer/index.html"
   },
   {
-    "revision": "2d60a724eaf99671cd93",
-    "url": "/Sorting-Visualizer/static/css/main.c6933b79.chunk.css"
+    "revision": "e81671847affb2efc4c6",
+    "url": "/Sorting-Visualizer/static/css/main.3ee79320.chunk.css"
   },
   {
     "revision": "12df00cf6a69522b241f",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Sorting-Visualizer/static/js/2.25af81b0.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "2d60a724eaf99671cd93",
-    "url": "/Sorting-Visualizer/static/js/main.f0d7987e.chunk.js"
+    "revision": "e81671847affb2efc4c6",
+    "url": "/Sorting-Visualizer/static/js/main.9a34bcea.chunk.js"
   },
   {
     "revision": "4b947d01b52864cbde3e",
